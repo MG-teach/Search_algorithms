e6aaf0a3ef157b73f7bc0b063bb5793155f79b4d
Emil Petkov <studio.vodopad@gmail.com>
2025-02-25 17:55:15 +0200

    zad2 completed

---
+++ zad2/BinarySearchMultiple.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class BinarySearchMultiple
+    {
+        public static List<int>? MultipleSearch(int[] array, int target)
+        {
+            List<int> indexes = new List<int>();
+            
+            int left = 0;
+            int right = array.Length - 1;
+            
+            while (left < right) 
+            {
+                int mid = left + (right - left) / 2;
+                if(array[mid] == target)
+                {
+                    indexes.Add(mid);
+                    break;
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            if (indexes.Count != 0)
+            {
+
+                int i = indexes[0];
+                indexes.Clear();
+                while (i >= 0 && array[i] == target)
+                {
+                    --i;
+                }
+
+                i++;
+
+                while (i < array.Length && array[i] == target)
+                {
+                    indexes.Add(i);
+                    i++;
+                }
+            }
+
+            return indexes.Count != 0 ? indexes : null;
+        }
+    }
+}
---
+++ zad2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете големината на масива: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете числото за търсене: ");
+            int target = int.Parse(Console.ReadLine());
+
+            bool sorted = true;
+            int[] array = ArrayCreator.CreateArray(size, sorted);
+
+            Console.WriteLine(string.Join(' ', array));
+
+            List<int>? result = BinarySearchMultiple.MultipleSearch(array, target);
+
+            if (result != null)
+            {
+                Console.WriteLine($"Числото е намерено {result.Count} пъти");
+                Console.WriteLine("Индексите на числото са: ");
+
+                foreach (int index in result)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Числото не е намерено.");
+            }
+        }
+    }
+}
