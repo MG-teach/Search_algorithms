73c687be4523cb0de3cd29ef6d444063ff164fc9
Emil Petkov <studio.vodopad@gmail.com>
2025-02-25 18:22:19 +0200

    zad3 completed

---
+++ zad3/LinearSearchTrasposition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    internal class LinearSearchTrasposition
+    {
+        public static int Search(int[] array, int target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    if (i > 0)
+                    {
+                        (array[i], array[i - 1]) = (array[i - 1], array[i]);
+                    }
+                    return i - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ zad3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете големината на масива: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете числото за търсене: ");
+            int target = int.Parse(Console.ReadLine());
+
+            bool sorted = false;
+            int[] array = ArrayCreator.CreateArray(size, sorted);
+
+            Console.WriteLine(string.Join(' ', array));
+
+            int result = LinearSearchTrasposition.Search(array, target);
+
+            if (result != -1)
+            {
+                Console.WriteLine($"Числото е намерено на индекс {result}.");
+            }
+            else
+            {
+                Console.WriteLine("Числото не е намерено.");
+            }
+            Console.WriteLine(string.Join(' ', array));
+        }
+    }
+}
