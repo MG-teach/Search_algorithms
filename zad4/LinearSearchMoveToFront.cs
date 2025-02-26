46c06f3e5ba531d86354b0a36ae4dd736e2dadcc
Emil Petkov <studio.vodopad@gmail.com>
2025-02-26 17:03:28 +0200

    zad4 ready

---
+++ zad4/LinearSearchMoveToFront.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class LinearSearchMoveToFront
+    {
+        public static int Search(int[] array, int target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    if (i > 0)
+                    {
+                        (array[i], array[0]) = (array[0], array[i]);
+                    }
+                    return 0;
+                }
+            }
+            return -1;
+        }
+    }
+}
